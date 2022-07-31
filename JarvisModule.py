ebb11be82f7cfea6f47a36a09fed46158a470568
Arman Jamshidi <97011229+Armanj88@users.noreply.github.com>
2022-07-31 14:49:31 +0430

    Add file that contains some important variables

---
+++ JarvisModule.py
@@ -0,0 +1,126 @@
+abilities = [
+    "open some apps",
+    "search on wikipedia and google",
+    "play music",
+    "summarize texts and files",
+    "get wifi passwords",
+    "get ip address",
+    "get weather",
+    "get date and time",
+    "get battery status",
+    "type something",
+    "read pdf",
+    "calculate something",
+    "check internet speed",
+    "send whatsapp message",
+    "tell you have to do something",
+    "tell you where are you",
+    "get news",
+    "tell jokes",
+    "shutdown computer",
+    "sleep computer",
+    "restart computer",
+    "change volume",
+    "generate password",
+    "convert audio to text",
+    "encrypt text",
+    "roll dice",
+    "open camera",
+    "set alarm",
+    "take screenshot",
+    "switch between windows",
+    "tell you some information about phone numbers",
+    "talk with you",
+]
+
+programs = {
+    "file explorer": "explorer",
+    "explorer": "explorer",
+    "notepad": "notepad",
+    "adobe photoshop": "photoshop",
+    "photoshop": "photoshop",
+    "adobe illustrator": "illustrator",
+    "illustrator": "illustrator",
+    "adobe premiere": "adobe premiere pro",
+    "premiere": "adobe premiere pro",
+    "microsoft edge": "msedge",
+    "msedge": "msedge",
+    "edge": "msedge",
+    "microsoft word": "winword",
+    "word": "winword",
+    "microsoft excel": "excel",
+    "excel": "excel",
+    "microsoft powerpoint": "powerpoint",
+    "powerpoint": "powerpoint",
+    "Visual studio code": "code",
+    "vscode": "code",
+    "code": "code",
+    "chrome": "chrome",
+    "google chrome": "chrome",
+    "brave": "brave",
+    "vlc": "vlc",
+    "media player": "Microsoft.Media.Player"
+}
+
+contacts = {
+    'arman': "+989191335617",
+    'alireza': "+989121335617",
+    'dad': "+989121335617",
+    'mom': "+989121599509",
+    'aida': "+989121599509",
+}
+
+sites_list = {
+    "school": "https://edu.daneshschools.com/my",
+    "school site": "https://edu.daneshschools.com/my",
+    "youtube": "www.youtube.com",
+    "facebook": "www.facebook.com",
+    "instagram": "www.instagram.com",
+    "twitter": "www.twitter.com",
+    "linkedin": "www.linkedin.com",
+    "github": "www.github.com",
+    "stackoverflow": "www.stackoverflow.com",
+    "stack overflow": "www.stackoverflow.com",
+    "quora": "www.quora.com",
+    "amazon": "www.amazon.com",
+    "ebay": "www.ebay.com",
+    "netflix": "www.netflix.com",
+    "spotify": "www.spotify.com",
+    "whatsapp": "www.whatsapp.com",
+    "gmail": "www.gmail.com",
+    "outlook": "www.outlook.com",
+    "yahoo": "www.yahoo.com",
+    "pinterest": "www.pinterest.com",
+    "tiktok": "www.tiktok.com",
+    "apple": "www.apple.com",
+    "microsoft": "www.microsoft.com",
+    "tesla": "www.tesla.com",
+    'whatsapp web': 'web.whatsapp.com',
+    'google maps': 'www.google.com/maps',
+}
+
+contacts_email = {
+    "arman": "jamshidiarman1388@gmail.com",
+    "nozha": "jamshidinozha1391@gmail.com"
+}
+
+alphabet_lower = "abcdefghijklmnopqrstuvwxyz"
+alphabet_upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+numbers = "0123456789"
+special_characters = "!@#$%^&*()_+-=[]{}|;':,./<>?"
+
+pass_chars = alphabet_lower + alphabet_upper + numbers + special_characters
+
+news_categories = [
+    'business',
+    'entertainment',
+    'environment',
+    'food',
+    'health',
+    'politics',
+    'science',
+    'sports',
+    'technology',
+    'top',
+    'world',
+]
